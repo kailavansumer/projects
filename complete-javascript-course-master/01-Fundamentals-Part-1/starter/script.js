48041024638236867130252b3312446cdfeab50f
Kaila VanSumer <kailajoy.vansumer@gmail.com>
2020-11-19 10:46:34 -0800

    type conversion and coercion

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -133,7 +133,18 @@ if (bmiMark > bmiJohn) {
 
 */
 
-//Type conversion (explicit change) and coercion
+//Type conversion (explicit change) and coercion (automatically done by js)
 
 const inputYear = "1991";
-console.log(inputYear + 18);
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+
+// Truthy and falsey values
