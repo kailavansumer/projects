b19ae523e44b2115b606d4dc1a57258bd8942b17
Kaila VanSumer <kailajoy.vansumer@gmail.com>
2020-11-19 10:06:38 -0800

    strings and template literals lesson

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -26,65 +26,63 @@ console.log("Is Mark's bmi higher than John's bmi?" + ' ' + isMarkFat); */
 // age <=21 ? console.log(firstName + ' drinks juice.')
 // : console.log(firstName + ' drinks beer.');
 
+// var teamJohnAverage = (89 + 120 + 103) / 3;
+// var teamMarkAverage = (116 + 94 + 123) / 3;
+// var teamMaryAverage = (97 + 134 + 105) / 3;
 
-var teamJohnAverage = (89 + 120 + 103)/3;
-var teamMarkAverage = (116 + 94 + 123)/3;
-var teamMaryAverage = (97 + 134 +105)/3;
+// if (teamJohnAverage > teamMarkAverage && teamJohnAverage > teamMaryAverage) {
+//   console.log("John's team is the winner with a score of " + teamJohnAverage);
+//   console.log("Mark's team only scored " + teamMarkAverage);
+//   console.log("Mary's team only scored " + teamMaryAverage);
+// } else if (
+//   teamMarkAverage > teamJohnAverage &&
+//   teamMarkAverage > teamMaryAverage
+// ) {
+//   console.log("Mark's team is the winner with a score of " + teamMarkAverage);
+//   console.log("John's team only scored " + teamJohnAverage);
+//   console.log("Mary's team only scored " + teamMaryAverage);
+// } else if (
+//   teamMaryAverage > teamJohnAverage &&
+//   teamMaryAverage > teamMarkAverage
+// ) {
+//   console.log("Mary's team is the winner with a score of " + teamMaryAverage);
+//   console.log("John's team only scored " + teamJohnAverage);
+//   console.log("Mark's team only scored " + teamMarkAverage);
+// } else if (
+//   teamJohnAverage === teamMaryAverage ||
+//   teamJohnAverage === teamMarkAverage
+// ) {
+//   console.log(
+//     "It's a draw with all teams scoring an average of " + teamJohnAverage
+//   );
+// }
 
+/*
+// Strings and Template Literals
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = "1991";
+const year = "2020";
 
-if (teamJohnAverage > teamMarkAverage && teamJohnAverage > teamMaryAverage) {
-    console.log('John\'s team is the winner with a score of ' + teamJohnAverage);
-    console.log('Mark\'s team only scored ' + teamMarkAverage);
-    console.log('Mary\'s team only scored ' + teamMaryAverage);
-}
-else if (teamMarkAverage > teamJohnAverage && teamMarkAverage > teamMaryAverage) {
-    console.log('Mark\'s team is the winner with a score of '+ teamMarkAverage);
-    console.log('John\'s team only scored ' + teamJohnAverage);
-    console.log('Mary\'s team only scored ' + teamMaryAverage);
-} 
-else if (teamMaryAverage > teamJohnAverage && teamMaryAverage > teamMarkAverage) {
-    console.log('Mary\'s team is the winner with a score of '+ teamMaryAverage);
-    console.log('John\'s team only scored ' + teamJohnAverage);
-    console.log('Mark\'s team only scored ' + teamMarkAverage);
-} 
+//Old way of concatonating strings
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
 
-else if (teamJohnAverage === teamMaryAverage || teamJohnAverage === teamMarkAverage) {
-    console.log('It\'s a draw with all teams scoring an average of ' + teamJohnAverage);
-}
+//ES6 now uses back ticks and the ${} notation to create complex strings
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
 
+//old way of writing multiple lines uses the \n\ due to a bug in the language
+const multipleLines = "String with \n\
+multiple \n\
+lines";
+console.log(multipleLines);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//ES6 now uses backticks and the return key to create multiple lines easier
+const multiLine = `String
+with
+multi
+lines`;
+console.log(multiLine);
+*/
