56e6710ae94df7254ba08b216f4cf331ea473ee7
Kaila VanSumer <kailajoy.vansumer@gmail.com>
2020-11-20 10:04:47 -0800

    moved portfolio repo

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,32 @@
+/* //Strict mode and errors
+
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive`);
+*/
+
+// Functions
+
+function logger() {
+  console.log(`My name is Jonas`);
+}
+
+// Calling, running, or invoking the function
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
