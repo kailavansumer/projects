d8142d5a2e5ab2eb00cedb8bee460f8675419f59
Kaila VanSumer <kailajoy.vansumer@gmail.com>
2020-11-20 17:22:56 -0800

    objects and methods lectures

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -168,3 +168,111 @@ const kaila = [firstName, lastName, age, friends];
 console.log(kaila);
 
 */
+
+/*
+const friends = ["Michael", "Aaron", "Joe"];
+
+// Add an element to the end of the array and store the value of the array in the variable newLength
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+// Add an element to the beginning of the array
+friends.unshift("John");
+console.log(friends);
+
+// Remove last element
+friends.pop();
+console.log(friends);
+
+// Remove first element
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("Aaron"));
+
+console.log(friends.includes("Steven"));
+
+*/
+
+/*
+// Function Coding Challenge 2
+
+const calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+
+const bills = [125, 556, 44];
+const tips = [
+  calcTip(bills[0]),
+  calcTip(bills[1]),
+  calcTip(bills[bills.length - 1]),
+];
+console.log(tips);
+
+const total = [
+  bills[0] + tips[0],
+  bills[1] + tips[1],
+  bills[bills.length - 1] + tips[tips.length - 1],
+];
+console.log(total);
+*/
+
+// Objects
+
+/*
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Guy",
+  age: "2020 - 1991",
+  job: "teacher",
+  friends: ["Michael", "Steven", "Joe"],
+};
+
+// Object Dot Notation vs Brackets
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["last" + nameKey]); // Calculated names only work with bracket notation, not dot notation
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends and his best friend is called ${jonas.friends[0]}.`
+);
+*/
+
+/*
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Guy",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Steven", "Joe"],
+  hasDriversLicense: false,
+  calcAge: function () {
+    return 2020 - this.birthYear;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} year old ${
+      this.job
+    } and has ${this.hasDriversLicense ? "a" : "no"} driver's license
+    .`;
+  },
+};
+
+// console.log(jonas.calcAge());
+// console.log(jonas["calcAge"](1991));
+
+// console.log(
+//   `${jonas.firstName} is a ${jonas.calcAge()} year old ${jonas.job} and has ${
+//     jonas.hasDriversLicense ? "a driver's license" : "no driver's license"
+//   }.`
+// );
+console.log(jonas.getSummary());
+*/
