8b260b1bb43913338fb40377aa11063f9725ab04
Kaila VanSumer <kailajoy.vansumer@gmail.com>
2020-12-04 14:25:08 -0800

    data structures practice and tutorial

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,20 @@
 'use strict';
-/*
+const weekdays = ['mon', 'tues', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -9,21 +24,6 @@ const restaurant = {
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
   },
 };
 
@@ -54,4 +54,85 @@ console.log(menu);
 
 const { sat, ...weekdays } = { ...restaurant.openingHours };
 console.log(weekdays);
+
+for (const item of menu) console.log(item);
+
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}: ${item[1]}`);
+}
+
+/*
+// Coding Challenge #1
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Create an array of players for each team
+const [players1, players2] = game.players;
+
+// Create a gk variable for first team player and an array for the remaining field players
+const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// Create an array of all players from both teams
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// Create new array including sub players for team 1
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+// Create variables for the odds
+const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// Create a function that prints the number of goals based on the number of arbitrary player names passed into the function (not an array)
+const printGoals = function (...playerNames) {
+  console.log(...playerNames);
+  console.log(playerNames.length);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmish');
+printGoals(...game.scored);
+
+const winner = team1 < team2 || 'Team 2 wins!';
+console.log(winner);
 */
