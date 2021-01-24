a3493e069ca7f792522409bd3410b90f67fcbc6f
Kaila VanSumer <kailajoy.vansumer@gmail.com>
2021-01-24 11:46:56 -0800

    added animation to mobile menu

--- practice/geeks-for-geeks/app.js
+++ practice/geeks-for-geeks/app.js
@@ -2,10 +2,16 @@ const navSlide = () => {
   const menu = document.querySelector(".menu");
   const nav = document.querySelector(".nav-links");
   const body = document.querySelector("body");
+  const line1 = document.querySelector(".line1");
+  const line2 = document.querySelector(".line2");
+  const line3 = document.querySelector(".line3");
 
   menu.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
     body.classList.toggle("overlay");
+    line1.classList.toggle("open");
+    line2.classList.toggle("open");
+    line3.classList.toggle("open");
   });
 };
 
