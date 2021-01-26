2099ba1c0baa2184bfbedbcaaf0fd303ff99a75f
Kaila VanSumer <kailajoy.vansumer@gmail.com>
2021-01-25 17:48:50 -0800

    refined js to make sure border was removed from all other tabs when a new active tab was clicked. Also changed the css animation so border comes up from bottom instead of sliding in left to right.

--- practice/youtube-user-header/app.js
+++ practice/youtube-user-header/app.js
@@ -1,10 +1,23 @@
-const activeLink = () => {
-  const navItem = document.querySelector(".nav-item");
-  const navLink = document.querySelector(".nav-link");
+"use strict";
 
-  navLink.addEventListener("click", () => {
-    navLink.classList.toggle("border");
-  });
+const navLink = [...document.querySelectorAll(".nav-link")];
+
+const toggleTab = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    array[i].addEventListener("click", function (e) {
+      let current = this;
+      for (let i = 0; i < array.length; i++) {
+        if (current != array[i]) {
+          array[i].classList.remove("border");
+        } else if (current.classList.contains("border")) {
+          current.classList.remove("border");
+        } else {
+          current.classList.add("border");
+        }
+      }
+      e.preventDefault();
+    });
+  }
 };
 
-activeLink();
+toggleTab(navLink);
